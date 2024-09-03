e91ae591968b9390895366b18185f1c5d18627fb
abasedape <Skylor808@gmail.com>
2024-09-03 09:43:06 -1000

    centered landing page - to be responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { Landingpage } from "@/components/component/landing page";
 
 export default function Home() {
-  return <Landingpage />;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Landingpage />
+    </div>
+  );
 }
